7c9262912a2ee603c297eb4754eac925e023a3bf
Johnson <johnmarios28@gmail.com>
2021-08-01 00:11:30 -0500

    primer comentario

---
+++ js/cookies.js
@@ -0,0 +1,17 @@
+function checkAcceptCookies() {
+    if (localStorage.acceptCookies == 'true') {} else {
+        $('#div-cookies').show();
+    }
+}
+
+function acceptCookies() {
+    localStorage.acceptCookies = 'true';
+    $('#div-cookies').hide();
+}
+$(document).ready(function() {
+    checkAcceptCookies();
+});
+
+
+
+
